7c74b617c28e614f3a9f38d578ec3cec235ebe00
Gareth Rogers <gareth@garethrogers.net>
2017-01-21 22:49:54 +0000

    Complete scrambling function, add rotate test cube

--- randominput.py
+++ randominput.py
@@ -1,15 +1,81 @@
-import random
+    import random
 
-# Because of how we read the data in,
-# the first index into the 3D array is the y-index (bottom layer first, top layer last),
-# then the z-index (back first, front last),
-# then the x-index (left first, right last)
-def getinput():
-  f = open('target.txt')
-  lists = [list(z) for y in [x.split('\n') for x in f.read().split('\n\n')] for z in y]
-  return [lists[i:i + 9] for i in xrange(0, len(lists), 9)]
+    # Because of how we read the data in,
+    # the first index into the 3D array is the y-index (bottom layer first, top layer last),
+    # then the z-index (back first, front last),
+    # then the x-index (left first, right last)
+    def getinput():
+        # f = open('target.txt')
+        # lists = [list(z) for y in [x.split('\n') for x in f.read().split('\n\n')] for z in y]
+        # return [lists[i:i + 9] for i in xrange(0, len(lists), 9)]
+        return [[['1', '2', '3', '4', '5', '6', '7', '8', '9'], ['2', '3', '4', '5', '6', '7', '8', '9', '1'], ['3', '4', '5', '6', '7', '8', '9', '1', '2'], ['4', '5', '6', '7', '8', '9', '1', '2', '3'], ['5', '6', '7', '8', '9', '1', '2', '3', '4'], ['6', '7', '8', '9', '1', '2', '3', '4', '5'], ['7', '8', '9', '1', '2', '3', '4', '5', '6'], ['8', '9', '1', '2', '3', '4', '5', '6', '7'], ['9', '1', '2', '3', '4', '5', '6', '7', '8']], [['2', '3', '4', '5', '6', '7', '8', '9', '1'], ['3', '4', '5', '6', '7', '8', '9', '1', '2'], ['4', '5', '6', '7', '8', '9', '1', '2', '3'], ['5', '6', '7', '8', '9', '1', '2', '3', '4'], ['6', '7', '8', '9', '1', '2', '3', '4', '5'], ['7', '8', '9', '1', '2', '3', '4', '5', '6'], ['8', '9', '1', '2', '3', '4', '5', '6', '7'], ['9', '1', '2', '3', '4', '5', '6', '7', '8'], ['1', '2', '3', '4', '5', '6', '7', '8', '9']], [['3', '4', '5', '6', '7', '8', '9', '1', '2'], ['4', '5', '6', '7', '8', '9', '1', '2', '3'], ['5', '6', '7', '8', '9', '1', '2', '3', '4'], ['6', '7', '8', '9', '1', '2', '3', '4', '5'], ['7', '8', '9', '1', '2', '3', '4', '5', '6'], ['8', '9', '1', '2', '3', '4', '5', '6', '7'], ['9', '1', '2', '3', '4', '5', '6', '7', '8'], ['1', '2', '3', '4', '5', '6', '7', '8', '9'], ['2', '3', '4', '5', '6', '7', '8', '9', '1']], [['4', '5', '6', '7', '8', '9', '1', '2', '3'], ['5', '6', '7', '8', '9', '1', '2', '3', '4'], ['6', '7', '8', '9', '1', '2', '3', '4', '5'], ['7', '8', '9', '1', '2', '3', '4', '5', '6'], ['8', '9', '1', '2', '3', '4', '5', '6', '7'], ['9', '1', '2', '3', '4', '5', '6', '7', '8'], ['1', '2', '3', '4', '5', '6', '7', '8', '9'], ['2', '3', '4', '5', '6', '7', '8', '9', '1'], ['3', '4', '5', '6', '7', '8', '9', '1', '2']], [['5', '6', '7', '8', '9', '1', '2', '3', '4'], ['6', '7', '8', '9', '1', '2', '3', '4', '5'], ['7', '8', '9', '1', '2', '3', '4', '5', '6'], ['8', '9', '1', '2', '3', '4', '5', '6', '7'], ['9', '1', '2', '3', '4', '5', '6', '7', '8'], ['1', '2', '3', '4', '5', '6', '7', '8', '9'], ['2', '3', '4', '5', '6', '7', '8', '9', '1'], ['3', '4', '5', '6', '7', '8', '9', '1', '2'], ['4', '5', '6', '7', '8', '9', '1', '2', '3']], [['6', '7', '8', '9', '1', '2', '3', '4', '5'], ['7', '8', '9', '1', '2', '3', '4', '5', '6'], ['8', '9', '1', '2', '3', '4', '5', '6', '7'], ['9', '1', '2', '3', '4', '5', '6', '7', '8'], ['1', '2', '3', '4', '5', '6', '7', '8', '9'], ['2', '3', '4', '5', '6', '7', '8', '9', '1'], ['3', '4', '5', '6', '7', '8', '9', '1', '2'], ['4', '5', '6', '7', '8', '9', '1', '2', '3'], ['5', '6', '7', '8', '9', '1', '2', '3', '4']], [['7', '8', '9', '1', '2', '3', '4', '5', '6'], ['8', '9', '1', '2', '3', '4', '5', '6', '7'], ['9', '1', '2', '3', '4', '5', '6', '7', '8'], ['1', '2', '3', '4', '5', '6', '7', '8', '9'], ['2', '3', '4', '5', '6', '7', '8', '9', '1'], ['3', '4', '5', '6', '7', '8', '9', '1', '2'], ['4', '5', '6', '7', '8', '9', '1', '2', '3'], ['5', '6', '7', '8', '9', '1', '2', '3', '4'], ['6', '7', '8', '9', '1', '2', '3', '4', '5']], [['8', '9', '1', '2', '3', '4', '5', '6', '7'], ['9', '1', '2', '3', '4', '5', '6', '7', '8'], ['1', '2', '3', '4', '5', '6', '7', '8', '9'], ['2', '3', '4', '5', '6', '7', '8', '9', '1'], ['3', '4', '5', '6', '7', '8', '9', '1', '2'], ['4', '5', '6', '7', '8', '9', '1', '2', '3'], ['5', '6', '7', '8', '9', '1', '2', '3', '4'], ['6', '7', '8', '9', '1', '2', '3', '4', '5'], ['7', '8', '9', '1', '2', '3', '4', '5', '6']], [['9', '1', '2', '3', '4', '5', '6', '7', '8'], ['1', '2', '3', '4', '5', '6', '7', '8', '9'], ['2', '3', '4', '5', '6', '7', '8', '9', '1'], ['3', '4', '5', '6', '7', '8', '9', '1', '2'], ['4', '5', '6', '7', '8', '9', '1', '2', '3'], ['5', '6', '7', '8', '9', '1', '2', '3', '4'], ['6', '7', '8', '9', '1', '2', '3', '4', '5'], ['7', '8', '9', '1', '2', '3', '4', '5', '6'], ['8', '9', '1', '2', '3', '4', '5', '6', '7']]]
 
-def output(data):
-  return '\n'.join([''.join(row) for layer in data for row in layer+['\n']])
+    def output(data):
+        return '\n'.join([''.join(row) for layer in data for row in layer+[[]]])
 
-print output(getinput())
+    def getsubcube(cube, x, y, z):
+        x = clamp(x, 1, 7)
+        y = clamp(y, 1, 7)
+        z = clamp(z, 1, 7)
+        return [[[char for char in row[x-1:x+2]] for row in layer[z-1:z+2]] for layer in cube[y-1:y+2]]
+
+    def replacesubcube(cube, subcube, x, y, z):
+        newcube = cube
+        for ypos in range(y-1, y+2):
+            for zpos in range(z-1, z+2):
+                for xpos in range(x-1, x+2):
+                    newcube[ypos][zpos][xpos] = subcube[ypos-y+1][zpos-z+1][xpos-x+1]
+        return newcube
+
+    def clamp(n, minn, maxn):
+        return max(min(n, maxn), minn)
+
+    def rotateX(cube, n):
+        newcube = cube
+        for _ in range(n):
+            newcube = [
+                [newcube[2][0], newcube[1][0], newcube[0][0]],
+                [newcube[2][1], newcube[1][1], newcube[0][1]],
+                [newcube[2][2], newcube[1][2], newcube[0][2]]
+            ]
+        return newcube
+
+    # algorithm shamelessly stolen from http://stackoverflow.com/a/496056/618347
+    def rotateY(cube, n):
+        newcube = cube
+        for _ in range(n):
+            newcube = [zip(*x)[::-1] for x in newcube]
+        return newcube
+
+    # Easier to define in terms of rotateX and rotateY
+    def rotateZ(cube, n):
+        newcube = cube
+        for _ in range(n):
+            newcube = rotateY(rotateX(rotateY(newcube, 1), 3), 3)
+        return newcube
+
+    def randomisecube(cube):
+        newcube = cube
+        generator = ""
+        solution = ""
+        funclist = [[rotateX, "X"], [rotateY, "Y"], [rotateZ, "Z"]]
+        for _ in range(random.randint(10, 100)):
+            x = random.randint(1, 7)
+            y = random.randint(1, 7)
+            z = random.randint(1, 7)
+            rotateFunc = random.choice(funclist)
+            numRots = random.randint(1,3)
+            subcube = getsubcube(newcube, x, y, z)
+            subcube = rotateFunc[0](subcube, numRots)
+            newcube = replacesubcube(newcube, subcube, x, y, z)
+            generator = generator + str(x) + str(y) + str(z) + rotateFunc[1] + str(numRots) + '\n'
+            solution = str(x) + str(y) + str(z) + rotateFunc[1] + str(4-numRots) +'\n' + solution
+        return [generator, solution, newcube]
+
+    answer = randomisecube(getinput())
+    print "Cube:"
+    print output(answer[2])
+    print "Generated by:"
+    print answer[0]
+    print "Possible solution:"
+    print answer[1]
